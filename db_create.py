8f7c626eb0ad7370e6279ca2f9b881e215b31811
egor <pojymar@gmail.com>
2022-05-11 02:33:12 +0300

    add database_create file

---
+++ db_create.py
@@ -0,0 +1,21 @@
+import psycopg2
+import yaml
+
+with open('host.yml') as config_file:
+    config = yaml.load(config_file, Loader=yaml.FullLoader)
+
+connection = psycopg2.connect(
+    host=config['host'],
+    user=config['user'],
+    password=config['password'],
+    database=config['database']
+)
+connection.autocommit = True
+data_sources = ["binance_data", "central_bank_data"]
+for i in data_sources:
+    with connection.cursor() as cursor:
+        cursor.execute(f"""
+                       CREATE TABLE {i}(
+                       id SERIAL PRIMARY KEY,
+                       time_received VARCHAR(20),
+                       price NUMERIC)""")
