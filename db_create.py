c8f8727afd9681bba6d026b481aad72a10cda2cf
egor <pojymar@gmail.com>
2022-05-11 19:28:03 +0300

    add 'create_table' func

--- db_create.py
+++ db_create.py
@@ -1,6 +1,7 @@
 import psycopg2
 import yaml
 
+
 with open('host.yml') as config_file:
     config = yaml.load(config_file, Loader=yaml.FullLoader)
 
@@ -11,11 +12,18 @@ connection = psycopg2.connect(
     database=config['database']
 )
 connection.autocommit = True
-data_sources = ["binance_data", "central_bank_data"]
-for i in data_sources:
-    with connection.cursor() as cursor:
-        cursor.execute(f"""
-                       CREATE TABLE {i}(
-                       id SERIAL PRIMARY KEY,
-                       time_received VARCHAR(20),
-                       price NUMERIC)""")
+
+
+def create_tables():
+    data_sources = ["binance_data", "central_bank_data"]
+    for i in data_sources:
+        with connection.cursor() as cursor:
+            cursor.execute(f"""
+                           CREATE TABLE {i}(
+                           id SERIAL PRIMARY KEY,
+                           time_received VARCHAR(26),
+                           price NUMERIC)""")
+
+
+if __name__ == '__main__':
+    create_tables()
