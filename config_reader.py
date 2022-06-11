e57b3645bdafb9bd7645e3476ea2bd671e7f78d8
egor <pojymar@gmail.com>
2022-06-11 06:07:56 +0300

    initial commit

---
+++ config_reader.py
@@ -0,0 +1,5 @@
+import yaml
+
+with open('config.yml') as config_file:
+    config = yaml.load(config_file, Loader=yaml.FullLoader)
+
---
+++ create_session.py
@@ -0,0 +1,4 @@
+from repo import SessionRepo
+
+if __name__ == '__main__':
+    SessionRepo.get_session('keyword', 'password')
