34d82dde0ba65d3f29964aa8a6ecdc6e58b2a124
egor <pojymar@gmail.com>
2022-06-13 17:51:25 +0300

    add notification

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import asyncio
 
 from extract import BinanceExtractor, CentralBankExtractor
-from repo import BinancePriceRepo, CentralBankRepo
+from repo import BinancePriceRepo, CentralBankRepo, config
 
 
 async def main():
     binance_repo = BinancePriceRepo()
     cb_repo = CentralBankRepo()
-    extractors = [BinanceExtractor(binance_repo), CentralBankExtractor(cb_repo)]
+    extractors = [
+        BinanceExtractor(binance_repo),
+        CentralBankExtractor(cb_repo)]
 
     for extractor in extractors:
-        asyncio.create_task(extractor.run(), name=extractor.__class__.__name__)
+        asyncio.create_task(
+            extractor.run(),
+            name=extractor.__class__.__name__)
 
     while True:
-        await asyncio.sleep(1)
+        await asyncio.sleep(config['NOTIFICATION_SECS_AWAIT'])
+
 
 if __name__ == '__main__':
-    asyncio.get_event_loop().run_until_complete(main())
+    asyncio.run(main())
+
+
